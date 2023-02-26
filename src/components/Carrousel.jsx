7209dc3fe40dccf3ec306d4159be1251bd54bac7
QuentinHeinis <quentin@heinis.dev>
2023-02-26 16:46:14 +0100

    update mainPage

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -1,48 +1,61 @@
 import Link from 'next/link'
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 
-const Items = ({ num }) => (
+const Items = ({ image, logo, type, name, tags }) => (
     <div className='h-full w-full relative flex items-center justify-center flex-none'>
-        <div className="flex flex-col items-center">
-            <div className='uppercase'>Identity</div>
-            <h2 className='text-3xl font-bold'>Osaka n°{num}</h2>
-            <Link href="/" className='bg-black bg-opacity-40 h-10 w-10 rounded-full flex items-center justify-center mt-4'>
-                <img src="/images/fleche.svg" alt="" className='' />
-                {/* <span className='h-[1px] w-4 bg-white block relative after:h-2 after:w-2  after:border-b after:border-r after:-rotate-45 after:top-1/2 after:right-0 after:-translate-y-1/2 after:absolute after:block '></span> */}
-            </Link>
+        <div className="flex flex-col items-center gap-2">
+            <div className='uppercase'>{type}</div>
+            <h2 className='text-6xl font-bold font-antigua'>{name}</h2>
+            <div className='uppercase flex gap-4 text-xs'>
+                {tags.map((item) => (
+                    <p className='border-white border rounded-full px-1 py-1' key={item}>{item}</p>
+
+                ))}
+            </div>
+            <img src={logo} alt={name + "'s Logo"} className='h-16' />
+            {/* <Link href="/" className='bg-red-500 bg-opacity-40 h-16 w-16 px-4 rounded-full flex items-center justify-center mt-4'>
+                <img src="/images/fleche.svg" alt="" className='h-full w-full' />
+            </Link> */}
         </div>
         <div className='absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center -z-10'>
             <img src="/images/cach.png" alt="" className='hidden lg:block absolute top-0 -translate-y-2/3' />
             <img src="/images/cach.png" alt="" className='hidden lg:block  absolute bottom-0 translate-y-2/3' />
-            <img src="/images/testD.png" alt="" className='object-cover h-full w-full lg:w-[90%]' />
+            <img src={image} alt="" className='object-cover h-full w-full lg:w-[90%]' />
         </div>
     </div>
 )
 const Carrousel = () => {
     const [nSelected, setNSelected] = useState(0)
-    const items = [1, 2, 3, 4]
-    useEffect(() => {
-        setInterval(() => {
-            if (nSelected == 0) { setNSelected(1) }
-            if (nSelected == 1) { setNSelected(2) }
-            if (nSelected == 2) { setNSelected(3) }
-            if (nSelected == 3) { setNSelected(0) }
-        }, 5000);
-    }, [nSelected])
+    const items = [{ n: 0, image: "/images/test2.png", logo: "/images/test2.png", type: "development", name: "TikTok", tags: ['web dev', 'webdesign'] },
+    { n: 1, image: "/images/test2.png", logo: "/images/test2.png", type: "development", name: "TikTok2", tags: ['web dev', 'webdesign'] },
+    { n: 2, image: "/images/test2.png", logo: "/images/test2.png", type: "development", name: "TikTok3", tags: ['web dev', 'webdesign'] }]
+
+
+    const next = () => {
+        if (nSelected < items.length - 1) {
+            setNSelected(prev => prev + 1)
+        } else {
+            setNSelected(0)
+        }
+    }
+
 
 
     return (
-        <div className="h-screen lg:h-[90vh] lg:pt-[15vh] w-screen mx-auto lg:w-[90vw]  overflow-hidden relative">
-            <div className='h-full flex transition-all duration-500' style={{ transform: `translate(-${nSelected * 100}%)` }} >
-                {items.map((item) => (
-                    <Items key={item} num={item} />
-                ))}
+        <div className='relative'>
+            <div className="h-screen lg:h-[90vh] lg:pt-[15vh] w-screen mx-auto lg:w-[90vw]  overflow-hidden relative">
+                <div className='h-full flex transition-all duration-500' style={{ transform: `translate(-${nSelected * 100}%)` }} >
+                    {items.map((item) => (
+                        <Items key={item.name} name={item.name} logo={item.logo} type={item.type} tags={item.tags} image={item.image} />
+                    ))}
+                </div>
+                <div className='absolute flex bottom-4 gap-2 right-1/2 translate-x-1/2'>
+                    {items.map((item) => (
+                        <span key={item.name} className='h-3 w-3 bg-white border cursor-pointer rounded-full' style={nSelected == item.n ? { backgroundColor: 'rgba(255,255,255,1)' } : { backgroundColor: 'rgba(255,255,255,0.4)' }} onClick={() => { setNSelected(item - 1) }}></span>
+                    ))}
+                </div>
             </div>
-            <div className='absolute flex bottom-4 gap-2 right-1/2 translate-x-1/2'>
-                {items.map((item) => (
-                    <span key={item} className='h-3 w-3 bg-white border rounded-full' style={nSelected == item - 1 ? { backgroundColor: 'rgba(255,255,255,1)' } : { backgroundColor: 'rgba(255,255,255,0.4)' }} onClick={() => { setNSelected(item - 1) }}></span>
-                ))}
-            </div>
+            <button type='button' onClick={next} className='uppercase text-2xl ml-[10vw]'>Next</button>
         </div>
     )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import Link from 'next/link'
+import React, { useState } from 'react'
 
 const Header = () => {
+    const [menuOpen, setmenuOpen] = useState(true)
+    const switchMenu = () => {
+        setmenuOpen((prev) => !prev)
+    }
     return (
         <header className='flex justify-between  pt-10 px-4 lg:px-24 w-screen fixed top-0 z-40'>
             <div className='flex lg:items-end flex-col lg:flex-row lg:gap-3'>
@@ -16,9 +21,48 @@ const Header = () => {
                     Web develloper and programmer
                 </div>
             </div>
-            <button className='h-5 w-5 bg-white'>
+            <div className='h-6 w-10 z-50 flex flex-col justify-between cursor-pointer relative' onClick={switchMenu} >
+                <span className={`w-full h-1 bg-red-400 absolute top-0 transition-all duration-500 ${!menuOpen && 'rotate-45 top-1/2'}`} style={menuOpen ? { backgroundColor: 'white', transform: "rotation(90deg)" } : { backgroundColor: 'black' }}></span>
+                <span className={`w-full h-1 bg-red-400 absolute bottom-0 transition-all duration-500 ${!menuOpen && 'rotate-[135deg] top-1/2'}`} style={menuOpen ? { backgroundColor: 'white' } : { backgroundColor: 'black' }}></span>
+            </div>
+            <div className='fixed top-0 bottom-0 right-0 left-0 bg-gray-100 flex flex-wrap justify-between items-center px-20 z-40 text-black uppercase transition-all duration-500' style={menuOpen ? { transform: 'translate(100%)' } : { transform: 'translate(0%)' }}>
+                <div className='flex flex-col gap-20'>
+                    <Link href="/" onClick={switchMenu}>
+                        <div className='flex gap-10 items-center'>
+                            <span className='text-xl'>01</span>
+                            <div className="relative slash border-b-2 hover:pb-2 flex ">
+                                <p className='text-9xl flex gap-4 pb-8 items-end leading-none relative z-10 slash0'>Works <img src="/images/flecheUp.svg" alt="" className='h-12 -translate-y-1/2' /></p>
+                                <span className='bg-gray-100 absolute h-0 flex top-1/2 scale-x-110 -translate-y-1/2 w-full z-40 transition-all after:transition-all slash4'></span>
 
-            </button>
+                                <span className='text-9xl flex gap-4 pb-8 items-end leading-none slash absolute top-0 transition-all slash1'>Works <img src="/images/flecheUp.svg" alt="" className='h-12 -translate-y-1/2' /></span>
+                                <span className='text-9xl flex gap-4 pb-8 items-end leading-none slash absolute top-0 transition-all slash2'>Works <img src="/images/flecheUp.svg" alt="" className='h-12 -translate-y-1/2' /></span>
+                            </div>
+                        </div>
+                    </Link>
+                    <Link href="/" onClick={switchMenu}>
+                        <div className='flex gap-10 items-center'>
+                            <span className='text-xl'>02</span>
+                            <div className="relative slash border-b-2 hover:pb-2 flex ">
+                                <p className='text-9xl flex gap-4 pb-8 items-end leading-none relative z-10 slash0'>Contact <img src="/images/flecheUp.svg" alt="" className='h-12 -translate-y-1/2' /></p>
+                                <span className='bg-gray-100 absolute h-0 flex top-1/2 scale-x-110 -translate-y-1/2 w-full z-40 transition-all after:transition-all slash4'></span>
+
+                                <span className='text-9xl flex gap-4 pb-8 items-end leading-none slash absolute top-0 transition-all slash1'>Contact <img src="/images/flecheUp.svg" alt="" className='h-12 -translate-y-1/2' /></span>
+                                <span className='text-9xl flex gap-4 pb-8 items-end leading-none slash absolute top-0 transition-all slash2'>Contact <img src="/images/flecheUp.svg" alt="" className='h-12 -translate-y-1/2' /></span>
+                            </div>
+                        </div>
+                    </Link>
+                </div>
+                <div className='flex flex-col gap-14'>
+                    <Link href='/'>
+                        <p className='text-3xl uppercase font-medium'>Contact</p>
+                        <span className='text-xl text-gray-400 normal-case'>E-mail</span>
+                    </Link>
+                    <Link href='/'>
+                        <p className='text-3xl uppercase font-medium'>Social media</p>
+                        <span className='text-xl text-gray-400 normal-case'>Linkedin</span>
+                    </Link>
+                </div>
+            </div>
         </header>
     )
 }
