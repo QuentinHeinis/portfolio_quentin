8eb2f07a0d3a27c31da7a44cf1506a223b6b429c
Quentin Heinis <94958477+QuentinHeinis@users.noreply.github.com>
2023-03-09 20:42:48 +0100

    add project

--- src/components/Gestion.jsx
+++ src/components/Gestion.jsx
@@ -1,46 +1,145 @@
 import useAuthStore from '@/store/authStore'
 import React, { useState } from 'react'
-import { getFirestore, collection, query, onSnapshot, orderBy } from 'firebase/firestore'
+import { getFirestore, collection, query, onSnapshot, orderBy, addDoc } from 'firebase/firestore'
+import { getStorage, ref, uploadString } from 'firebase/storage'
+import { async } from '@firebase/util'
+import { useRouter } from 'next/router'
 
 const Gestion = ({ props }) => {
+    const router = useRouter()
     const [listeProjects, setListeProjects] = useState(props)
-    const { removeUser } = useAuthStore()
+    const [AddProject, setAddProject] = useState(true)
 
+    const [imagesData, setimagesData] = useState()
+    const [logoData, setlogoData] = useState()
+    const [desc, setdesc] = useState('')
+    const [title, settitle] = useState('')
+    const [langage, setlangage] = useState('')
+    const [year, setyear] = useState('')
+    const [type, settype] = useState('')
+    const [tags, settags] = useState('')
+    const [isHomepage, setisHomepage] = useState()
+    const { removeUser } = useAuthStore()
+    const upload = async (e) => {
+        e.preventDefault()
+        let imagesPath = []
+        const storage = getStorage()
+        let i = 0
+        for (let i = 0; i < imagesData.length; i++) {
+            const refStorage = ref(storage, `${title}/` + title + i)
+            await uploadString(refStorage, imagesData[i], 'data_url').then((snapshot) => {
+                imagesPath.push(snapshot.metadata.fullPath)
+            })
+        }
+        let logoPath = ''
+        const refStorage = ref(storage, `${title}/` + title + "Logo")
+        await uploadString(refStorage, logoData, 'data_url').then((snapshot) => {
+            logoPath = snapshot.metadata.fullPath
+        })
+        let projet = {
+            images: imagesPath,
+            logo: logoPath,
+            desc: desc.replace("\n", "\\n"),
+            title: title,
+            langage: langage,
+            year: year,
+            type: type,
+            tags: tags.split('\n'),
+            n: isHomepage
+        }
+        const db = getFirestore();
+        const docRef = await addDoc(collection(db, 'projets'), projet);
+        router.push("/projet" + docRef.id)
+    }
+
+    const SetImagesData = (e) => {
+        const files = e.target.files
+        let images = []
+        for (let i = 0; i < files.length; i++) {
+            var reader = new FileReader()
+            reader.onload = (e) => {
+                images.push(e.target.result)
+            }
+            reader.readAsDataURL(files[i])
+        }
+        console.log(images)
+        setimagesData(images)
+    }
+    const SetLogoData = (e) => {
+        const files = e.target.files
+        var reader = new FileReader()
+        reader.onload = (e) => {
+            setlogoData(e.target.result)
+        }
+        reader.readAsDataURL(files[0])
+    }
     return (
         <>
-            <button onClick={() => removeUser()} className='pt-40'>se déconnecter</button>
-            <div className='w-screen '>
-                <table className='mx-auto max-w-full'>
-                    <thead className='h-14'>
-                        <tr>
-                            <td className='border border-white w-28 text-center'>name</td>
-                            <td className='border border-white w-28 text-center'>logo</td>
-                            <td className='border border-white w-28 text-center'>images</td>
-                            <td className='border border-white w-28 text-center'>tags</td>
-                            <td className='border border-white w-28 text-center'>type</td>
-                            <td className='border border-white w-28 text-center'>description</td>
-                            <td className='border border-white w-28 text-center'>n</td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {listeProjects.map((projet) => (
-                            <tr key={projet.id}>
-                                <td className='border border-white w-28 text-center'>{projet.name}</td>
-                                <td className='border border-white w-28'><img src={projet.logo} alt="" className='max-w-xs mx-auto' /></td>
-                                <td className='border border-white w-28 text-center'>{projet.images.map(image => (
-                                    <img src={image} key={image} alt="" className='max-w-xs' />
-                                ))}</td>
-                                <td className='border border-white w-28 text-center'>{projet.tags.map(tag => (
-                                    <p key={tag}>{tag}</p>
-                                ))}</td>
-                                <td className='border border-white w-28 text-center'>{projet.type}</td>
-                                <td className='border border-white w-28 text-center'>{projet.desc}</td>
-                                <td className='border border-white w-28 text-center'>{projet.n}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
+            <div className='flex justify-between w-full pt-40 px-14'>
+                <button onClick={() => removeUser()} >se déconnecter</button>
+                {AddProject ? <button onClick={() => setAddProject(prev => !prev)} >Add</button> :
+                    <button onClick={() => setAddProject(prev => !prev)} >Cancel</button>}
             </div>
+            {AddProject ?
+                <div className='w-screen '>
+                    <h1 className='text-2xl text-center pb-10'>Projets</h1>
+                    <div className="flex max-w-full overflow-x-scroll px-20">
+                        <table className='mx-auto max-w-full'>
+                            <thead className='h-14'>
+                                <tr>
+                                    <td className='border border-white w-28 text-center'>name</td>
+                                    <td className='border border-white w-28 text-center'>logo</td>
+                                    <td className='border border-white w-28 text-center'>images</td>
+                                    <td className='border border-white w-28 text-center'>tags</td>
+                                    <td className='border border-white w-28 text-center'>type</td>
+                                    <td className='border border-white w-28 text-center'>langage/logiciel</td>
+                                    <td className='border border-white w-28 text-center'>description</td>
+                                    <td className='border border-white w-28 text-center'>n</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {listeProjects.map((projet) => (
+                                    <tr key={projet.id}>
+                                        <td className='border border-white w-28 text-center'>{projet.title}</td>
+                                        <td className='border border-white w-28'><img src={projet.logo} alt="" className='max-w-xs mx-auto' /></td>
+                                        <td className='border border-white w-28 text-center'>{projet.images.map(image => (
+                                            <img src={image} key={image} alt="" className='max-w-xs' />
+                                        ))}</td>
+                                        <td className='border border-white w-28 text-center'>{projet.tags.map(tag => (
+                                            <p key={tag}>{tag}</p>
+                                        ))}</td>
+                                        <td className='border border-white w-28 text-center'>{projet.type}</td>
+                                        <td className='border border-white w-28 text-center'>{projet.langage}</td>
+                                        <td className='border border-white w-28 text-center'>{projet.desc.replace('\\n', '\n')}</td>
+                                        <td className='border border-white w-28 text-center'>{projet.n}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div> :
+                <div>
+                    <h1 className='text-center text-2xl'>Ajouter un projet</h1>
+                    <form onSubmit={upload} className="text-black">
+                        <div className="flex flex-col w-1/3 gap-3 mx-auto">
+                            <div className="flex">
+                                <label></label>
+                                <input type="file" accept='image/png,image/jpeg,image/webp,image/gif' multiple onChange={SetImagesData} required />
+                            </div>
+                            <input type="file" accept='image/png,image/jpeg,image/webp,image/gif' onChange={SetLogoData} required />
+                            <input type="text" placeholder='Titre' onChange={e => settitle(e.target.value)} required />
+                            <input type="text" placeholder='Logiciel/langage' onChange={e => setlangage(e.target.value)} required />
+                            <input type="text" placeholder='Année' onChange={e => setyear(e.target.value)} required />
+                            <input type="text" placeholder='Type de projet' onChange={e => settype(e.target.value)} required />
+                            <textarea placeholder='Description' onChange={e => setdesc(e.target.value)} required></textarea>
+                            <textarea placeholder='tags' onChange={e => settags(e.target.value)} required></textarea>
+                            <input type="number" placeholder='n' onChange={e => setisHomepage(e.target.value)} />
+
+                            <button className='bg-white'>Upload</button>
+                        </div>
+                    </form>
+                </div>
+            }
         </>
     )
 }
--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,7 +1,8 @@
 import Gestion from '@/components/Gestion'
 import Login from '@/components/Login'
 import useAuthStore from '@/store/authStore'
-import { getFirestore, collection, query, onSnapshot, orderBy, getDocs } from 'firebase/firestore'
+import { getFirestore, collection, query, getDocs, where } from 'firebase/firestore'
+import { getDownloadURL, getStorage, ref } from 'firebase/storage'
 import React from 'react'
 
 const Admin = ({ data }) => {
@@ -14,12 +15,30 @@ const Admin = ({ data }) => {
 }
 export const getServerSideProps = async () => {
     let data = []
+    let temp = []
     const firestore = getFirestore()
-    const querySnapshot = await getDocs(collection(firestore, "projets"));
+    let q = query(collection(firestore, "projets"))
+    const querySnapshot = await getDocs(q);
     querySnapshot.forEach((doc) => {
-        let tempData = { id: doc.id, ...doc.data() }
-        data.push(tempData)
+        temp.push({ id: doc.id, ...doc.data() })
     });
+    for (let i = 0; i < temp.length; i++) {
+        const storage = getStorage();
+        for (let j = 0; j < temp[i].images.length; j++) {
+            const spaceRef = ref(storage, temp[i].images[j]);
+            await getDownloadURL(spaceRef)
+                .then((url) => {
+                    temp[i].images[j] = url;
+                })
+        }
+        const spaceRef = ref(storage, temp[i].logo);
+        await getDownloadURL(spaceRef)
+            .then((url) => {
+                temp[i].logo = url;
+            })
+        data.push(temp[i])
+    }
+
     return {
         props: { data }
     }
