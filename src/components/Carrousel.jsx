26b316225bcbafa49070952333bc890ffaeee135
Quentin Heinis <94958477+QuentinHeinis@users.noreply.github.com>
2023-03-09 22:40:26 +0100

    fix carrousel homepage

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -2,9 +2,9 @@ import { getDownloadURL, getStorage, ref } from 'firebase/storage'
 import Link from 'next/link'
 import React, { useState } from 'react'
 
-const Items = ({ image, logo, type, name, tags }) => (
+const Items = ({ id, image, logo, type, name, tags }) => (
     <div className='h-full w-full relative flex items-center justify-center flex-none'>
-        <div className="flex flex-col items-center gap-2">
+        <Link href={'/projet/' + id} className="flex flex-col items-center gap-2 cursor-pointer">
             <div className='uppercase'>{type}</div>
             <h2 className='text-6xl font-bold font-antigua'>{name}</h2>
             <div className='uppercase flex gap-4 text-xs'>
@@ -17,11 +17,11 @@ const Items = ({ image, logo, type, name, tags }) => (
             {/* <Link href="/" className='bg-red-500 bg-opacity-40 h-16 w-16 px-4 rounded-full flex items-center justify-center mt-4'>
                 <img src="/images/fleche.svg" alt="" className='h-full w-full' />
             </Link> */}
-        </div>
+        </Link>
         <div className='absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center -z-10'>
             <img src="/images/cach.png" alt="" className='hidden lg:block absolute top-0 -translate-y-2/3' />
             <img src="/images/cach.png" alt="" className='hidden lg:block  absolute bottom-0 translate-y-2/3' />
-            <img src={image} alt="" className='object-cover h-full w-full lg:w-[90%]' />
+            <img src={image} alt={'photo du projet ' + name} className='object-cover h-full w-full lg:w-[90%]' />
         </div>
     </div>
 )
@@ -44,7 +44,7 @@ const Carrousel = ({ props }) => {
             <div className="h-screen lg:h-[90vh] lg:pt-[15vh] w-screen mx-auto lg:w-[90vw]  overflow-hidden relative">
                 <div className='h-full flex transition-all duration-500' style={{ transform: `translate(-${nSelected * 100}%)` }} >
                     {items.map((item) => (
-                        <Items key={item.title} name={item.title} logo={item.logo} type={item.type} tags={item.tags} image={item.images[0]} />
+                        <Items key={item.title} id={item.id} name={item.title} logo={item.logo} type={item.type} tags={item.tags} image={item.images[0]} />
                     ))}
                 </div>
                 <div className='absolute flex bottom-4 gap-2 right-1/2 translate-x-1/2'>
--- src/pages/projet/[id].js
+++ src/pages/projet/[id].js
@@ -1,9 +1,107 @@
-import React from 'react'
+import { doc, getDoc, getFirestore } from 'firebase/firestore'
+import { getDownloadURL, getStorage, ref } from 'firebase/storage'
+import React, { useState } from 'react'
+import { useRouter } from 'next/router'
 
-const Detail = () => {
+const Detail = ({ data }) => {
+    const router = useRouter()
+    const [index, setIndex] = useState(0)
+    if (!data) {
+        router.push('/')
+    }
+
+    const [nSelected, setNSelected] = useState(0)
+    const next = () => {
+        if (nSelected < data.images.length - 1) {
+            setNSelected(prev => prev + 1)
+        } else {
+            setNSelected(0)
+        }
+    }
     return (
-        <div>Detail</div>
+        <div>
+            <div className='hidden md:flex px-5 h-[90vh] pt-20'>
+                <div className='w-4/5 h-full'>
+                    <img src={data.images && data.images[index]} className='max-h-full w-full object-contain h-full' />
+                </div>
+                <div className='w-1/5 flex flex-col justify-between'>
+                    {data.images?.map((item, i) => (
+                        <img key={i} src={item} className={i === index ? 'hidden' : 'block w-full h-1/6 object-contain cursor-pointer'} onClick={() => setIndex(i)} />
+                    ))}
+                </div>
+            </div>
+            <div className="h-screen lg:h-[90vh] lg:pt-[15vh] w-screen mx-auto lg:w-[90vw]  overflow-hidden relative md:hidden">
+                <div className='h-full flex transition-all duration-500' style={{ transform: `translate(-${nSelected * 100}%)` }} >
+                    {data.images.map((image) => (
+                        <div key={image} className="h-full w-full relative flex items-center justify-center flex-none">
+                            <img src={image} className='object-cover h-full w-full lg:w-[90%]' />
+                        </div>
+                    ))}
+                </div>
+                <div className='absolute flex bottom-4 gap-2 right-1/2 translate-x-1/2 md:hidden'>
+                    {data.images.map((item) => (
+                        <span key={item} className='h-3 w-3 bg-white border cursor-pointer rounded-full'></span>
+                    ))}
+                </div>
+            </div>
+            <button type='button' onClick={next} className='uppercase text-2xl ml-[10vw] absolute bottom-0 md:hidden'>Next</button>
+            <div className='flex flex-col w-4/5 mx-auto'>
+                <p className='text-[#757575] uppercase text-2xl mt-10'>{data.type} Project</p>
+                <h1 className='text-white uppercase text-4xl mt-10'>{data.title} - {data.year}</h1>
+                <div class="flex w-full justify-between mt-24">
+                    <h2 className='text-[#757575] uppercase text-2xl'>Language/Software used</h2>
+                    <span className='text-[#757575] uppercase text-2xl'>01</span>
+                </div>
+                <p className='text-2xl pt-6'>{data.langage}</p>
+                <span className='w-full h-[2px] bg-[#757575] my-6'></span>
+                <div class="flex w-full justify-between">
+                    <h3 className='text-[#757575] uppercase text-2xl'>Year</h3>
+                    <span className='text-[#757575] uppercase text-2xl'>02</span>
+                </div>
+                <p className='text-2xl pt-6'>{data.year}</p>
+                <span className='w-full h-[2px] bg-[#757575] my-6'></span>
+                <div class="flex w-full justify-between mb-6">
+                    <h3 className='text-[#757575] uppercase text-2xl'>Project Description</h3>
+                    <span className='text-[#757575] uppercase text-2xl '>03</span>
+                </div>
+                {data.desc.split('\\n').map((paragraph) => (
+
+                    <p className='text-2xl first-letter:uppercase'>{paragraph}</p>
+                ))}
+                <span className='w-full h-[2px] bg-[#757575] my-6'></span>
+                <p className='text-2xl uppercase'>Copyright &copy; Quentin Heinis</p>
+            </div>
+        </div>
     )
+}
+
+export const getServerSideProps = async ({ params: { id } }) => {
+    let data = null
+    let temp = null
+    const firestore = getFirestore();
+    const docRef = doc(firestore, "projets", id);
+    temp = await getDoc(docRef);
+
+    if (temp.exists()) {
+        data = temp.data()
+        const storage = getStorage();
+        for (let i = 0; i < data.images.length; i++) {
+            const spaceRef = ref(storage, data.images[i]);
+            await getDownloadURL(spaceRef)
+                .then((url) => {
+                    data.images[i] = url;
+                })
+        }
+        const spaceRef = ref(storage, data.logo);
+        await getDownloadURL(spaceRef)
+            .then((url) => {
+                data.logo = url;
+            })
+    }
+
+    return {
+        props: { data }
+    }
 }
 
 export default Detail
